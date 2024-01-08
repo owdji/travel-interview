e11e79fdd9f1d64947a06daacf5de9369c95252f
lchollet <leo.chollet.2000@gmail.com>
2024-01-08 10:06:00 +0100

    add register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,4 +9,8 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register.page').then( m => m.RegisterPage)
+  },
 ];
