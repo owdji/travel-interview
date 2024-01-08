5e0a9b19a7fec06ad30f2cee08b2434a8c63f92d
lchollet <leo.chollet.2000@gmail.com>
2024-01-08 16:06:39 +0100

    adding register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, HttpClientModule],
 })
 export class AppComponent {
   constructor() {}
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,20 +1,41 @@
+// Import necessary modules and HttpClient
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class RegisterPage implements OnInit {
+  data: any = {};
+  name: String;
+  password: String;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    this.name = '';
+    this.password = '';
   }
 
+  ngOnInit() {}
+
+  postData(name: String, password: String) {
+    const url = 'https://comem-travel-log-api-2hr8.onrender.com/api/users';
+    const body = {
+      name: name,
+      password: password,
+    };
+    // create a console log to see if the data is correct
+    console.log(body);
+    // send the data to the API
+    this.http.post(url, body).subscribe(
+      (data) => console.log(data),
+      (error) => console.error('Erreur lors de la requête POST:', error)
+    );
+  }
 }
