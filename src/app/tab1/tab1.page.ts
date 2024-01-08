f96aab37fd02abf4eebe419a14196d5f8cdbbe6a
owdji <omar.gonin@gmail.com>
2024-01-08 11:33:16 +0100

    ajout du ion fab

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonFab, IonFabButton, IonIcon, IonFabList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFab,IonFabButton, IonIcon, IonFabList]
 })
 export class Tab1Page {
   constructor() {}
