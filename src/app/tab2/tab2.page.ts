06143ffdf299b29ef6061d66f94d6690527cbabd
Andrea Ferrer <andrea.ferrer@heig-vd.ch>
2024-01-15 14:55:31 +0100

    map geolocalisation

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,30 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import Map from 'ol/Map';
 import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
-
+import { Point } from 'ol/geom';
+import { Geolocation } from 'ol';
+import Overlay from 'ol/Overlay';
+import { transform } from 'ol/proj';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, CommonModule, IonButton]
 })
 
 export class Tab2Page implements OnInit {
+  geolocationEnabled = false;
+  geolocation: Geolocation;
+  marker: HTMLElement | null = null;
 
-  constructor() {}
+  constructor() {
+    this.geolocation = new Geolocation({
+      trackingOptions: {
+        enableHighAccuracy: true,
+      },
+      projection: 'EPSG:3857',
+    });
+  }
 
   ngOnInit() {
-    var map = new Map({
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }) ],
+    const map = new Map({
+      layers: [new TileLayer({ source: new OSM() })],
       target: 'map',
       view: new View({
         center: [0, 0],
@@ -32,11 +43,37 @@ export class Tab2Page implements OnInit {
       }),
     });
 
-    //Update size of map to fix load error
-		setTimeout(() => {
-			map.updateSize();
-		}, 1500);
+    // Update size of map to fix load error
+    setTimeout(() => {
+      map.updateSize();
+    }, 1500);
 
+    this.marker = document.getElementById('marker');
+
+    const mapView = map.getView();
+
+    // Initialize the Geolocation instance
+    this.geolocation = new Geolocation({
+      trackingOptions: {
+        enableHighAccuracy: true,
+      },
+      projection: mapView.getProjection(),
+    });
   }
 
+  toggleGeolocation() {
+    if (this.geolocation) {
+      if (!this.geolocationEnabled) {
+        console.log('Geolocation enabled');
+        // Start tracking the user's position
+        this.geolocation.setTracking(true);
+      } else {
+        console.log('Geolocation disabled');
+        // Stop tracking the user's position
+        this.geolocation.setTracking(false);
+      }
+      this.geolocationEnabled = !this.geolocationEnabled;
+    }
+    
+  }
 }
