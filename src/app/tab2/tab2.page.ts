d6215ad87cdc6a19a023d151d62341f6e46d20d3
Andrea Ferrer <andrea.ferrer@heig-vd.ch>
2024-01-15 11:37:49 +0100

    map added

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+
 
 @Component({
   selector: 'app-tab2',
@@ -9,8 +14,29 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
 })
-export class Tab2Page {
+
+export class Tab2Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit() {
+    var map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }) ],
+      target: 'map',
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+
+    //Update size of map to fix load error
+		setTimeout(() => {
+			map.updateSize();
+		}, 1500);
+
+  }
 
 }
