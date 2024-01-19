62e62ba6918f2038e130713a2113e6e384678e43
lchollet <leo.chollet.2000@gmail.com>
2024-01-19 16:05:29 +0100

    adding details to places page

--- src/app/models/trip-response.type.ts
+++ src/app/models/trip-response.type.ts
@@ -1,8 +1,11 @@
 export type TripResponse = {
+    createdAt: string;
     id: string;
     title: string;
     description: string;
-    // search: string;
+    placesCount: number;
+    userId: string;
+
     // sort: string;
     // href: string;
 
