8a312e8ec9764ab7db3b33fb3931b5515cb79b87
lchollet <leo.chollet.2000@gmail.com>
2024-01-28 14:40:41 +0100

    add of redirection and user message on register page

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -17,7 +19,7 @@ export class RegisterPage implements OnInit {
   name: String;
   password: String;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, public toastController: ToastController, private router: Router) {
     this.name = '';
     this.password = '';
   }
@@ -30,12 +32,38 @@ export class RegisterPage implements OnInit {
       name: name,
       password: password,
     };
+
     // create a console log to see if the data is correct
     console.log(body);
+
     // send the data to the API
     this.http.post(url, body).subscribe(
-      (data) => console.log(data),
-      (error) => console.error('Erreur lors de la requête POST:', error)
+      (data) => {
+        console.log(data);
+
+        // Ajouter un message à l'utilisateur
+        this.presentToast('Votre compte a été créé avec succès ! Vous pouvez maintenant vous connecter.');
+
+        // Rediriger l'utilisateur vers la page de login (assurez-vous d'avoir la bonne URL)
+        this.router.navigateByUrl('/login');
+      },
+      (error) => {
+        console.error('Erreur lors de la requête POST:', error);
+
+        // Ajouter un message d'erreur à l'utilisateur
+        alert(
+          'Erreur lors de la création du compte. Veuillez réessayer plus tard.'
+        );
+      }
     );
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 }
