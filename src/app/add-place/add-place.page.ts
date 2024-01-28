da0c56eea08adb1a7b75be8e4942dcd22b270652
lchollet <leo.chollet.2000@gmail.com>
2024-01-28 22:49:26 +0100

    add validation error to create-place

--- src/app/add-place/add-place.page.ts
+++ src/app/add-place/add-place.page.ts
@@ -7,6 +7,8 @@ import { PlacesService } from '../services/places-service.service';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { PlaceResponse } from '../models/place-response.type';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-place',
@@ -24,7 +26,9 @@ export class AddPlacePage implements OnInit {
   constructor(
     private placesService: PlacesService,
     private modalController: ModalController,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private toastController: ToastController,
+    private router: Router
   ) {}
 
   closeModal() {
@@ -43,35 +47,59 @@ export class AddPlacePage implements OnInit {
           coordinates: [formData.X, formData.Y],
         },
       };
-  
-      this.placesService.postPlace(place).pipe(
-        takeUntil(this.unsubscribe$)
-      ).subscribe({
-        next: (createdPlace) => console.log('Place created:', createdPlace),
-        error: (err) => {
-          if (err.status === 422) {
-            // Handle validation error
-            this.presentValidationError('The title must be at least 3 characters long and the description must be at least 5 characters long.');
-          } else {
-            console.log(err);
-          }
-        },
-      });
+
+      this.placesService
+        .postPlace(place)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe({
+          next: (createdPlace) => {
+            console.log('Place created:', createdPlace);
+            this.presentSuccessToast(); // Display success toast
+            this.closeModal(); // Close the modal
+            location.reload(); // Reload the page
+          },
+          error: (err) => {
+            if (err.status === 422) {
+              // Handle validation error
+              this.presentValidationError(
+                'The title must be at least 3 characters long and the description must be at least 5 characters long.'
+              );
+            } else {
+              console.log(err);
+            }
+          },
+        });
     } else {
       this.presentAlert();
     }
   }
-  
+
   private async presentValidationError(message: string) {
     const alert = await this.alertController.create({
       header: 'Validation Error',
       message: message,
       buttons: ['OK'],
     });
-  
+
     await alert.present();
   }
-  
+
+  private async presentSuccessToast() {
+    const toast = await this.toastController.create({
+      message: 'Place created successfully!',
+      duration: 2000, // Display for 2 seconds
+      position: 'bottom',
+      color: 'success',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    await toast.present();
+  }
 
   async presentAlert() {
     const alert = await this.alertController.create({
