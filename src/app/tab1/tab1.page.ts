1c05d2fd98e27a3a622d92c59abad9abbc03b6ba
Andrea Ferrer <andrea.ferrer@heig-vd.ch>
2024-01-28 18:29:21 +0100

    adding new properties

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent ,IonText ,IonList ,IonItemSliding ,IonItemOptions, IonItem ,IonLabel } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
@@ -9,31 +9,54 @@ import { IonFab, IonFabButton, IonIcon, IonFabList, IonButton,  IonButtons} from
 import { AuthService } from '../security/auth.service';
 import { logOut as logOutIcon } from "ionicons/icons";
 import { Router } from "@angular/router";
-
+import { TripService } from '../services/trips-service.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFab,IonFabButton, IonIcon, IonFabList, IonButton, IonButtons]
+  imports: [IonLabel, IonItemOptions, IonItemSliding, IonList, IonText, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFab,IonFabButton, IonIcon, IonFabList, IonButton, IonButtons, IonItem, CommonModule]
 })
 export class Tab1Page {
+  trips: TripResponse[];
   readonly logOutIcon = logOutIcon;
 
   constructor(
+    private tripService: TripService,
     // Inject the authentication provider.
     private auth: AuthService,
     // Inject the router
-    private router: Router
-  ) {}
+    private router: Router,
+  ) {
+    this.trips = [];
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getTrips();
+  }
 
   // Add a method to log out.
   logOut() {
     console.log("logging out...");
+    //create an alert to confirm the user wants to log out
+    const isConfirmed = confirm("Are you sure you want to log out?");
+    if (!isConfirmed) {
+      return;
+    }
     this.auth.logOut();
     this.router.navigateByUrl("/login");
+  }
+
+  getTrips() {
+    this.tripService.getTrips().subscribe({
+      next: (trips) => (this.trips = trips),
+      error: (err) => console.log(err),
+    });
+  }
+
+  caMarche(trip: any) {
+    console.log("ca marche", trip);
+    this.trips.push(trip);
   }
 }
